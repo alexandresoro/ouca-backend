2b064ae31fbcafbf210b1d7cb567017a44d51214
Alexandre Soro <code@soro.dev>
2025-01-05 18:14:05 +0100

    feat(fastify): add response validation for search species

--- packages/backend/src/application/http/controllers/search-controller.ts
+++ packages/backend/src/application/http/controllers/search-controller.ts
@@ -3,6 +3,7 @@ import type { FastifyPluginCallbackZod } from "fastify-type-provider-zod";
 import { Result } from "neverthrow";
 import type { Services } from "../../services/services.js";
 import { withAuthenticationErrorResponses } from "../hooks/handle-authorization-hook.js";
+import { buildFastifyDefaultErrorResponses } from "./api-utils.js";
 import { getPaginationMetadata } from "./controller-utils.js";
 
 export const searchController: FastifyPluginCallbackZod<{
@@ -17,7 +18,10 @@ export const searchController: FastifyPluginCallbackZod<{
         security: [{ token: [] }],
         tags: ["Species"],
         querystring: getSpeciesQueryParamsSchema,
-        response: withAuthenticationErrorResponses({}),
+        response: withAuthenticationErrorResponses({
+          200: getSpeciesPaginatedResponse,
+          ...buildFastifyDefaultErrorResponses([403]),
+        }),
       },
     },
     async (req, reply) => {
@@ -45,12 +49,10 @@ export const searchController: FastifyPluginCallbackZod<{
 
       const [speciesData, count] = paginatedResults.value;
 
-      const response = getSpeciesPaginatedResponse.parse({
+      return await reply.send({
         data: speciesData,
         meta: getPaginationMetadata(count, req.query),
       });
-
-      return await reply.send(response);
     },
   );
 
