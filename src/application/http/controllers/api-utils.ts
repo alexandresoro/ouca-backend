1a481a872ef7f47f0b32767da35a262ecc5f949a
Alexandre Soro <code@soro.dev>
2025-02-11 22:28:25 +0100

    feat(server): remove fastify controllers

--- src/application/http/controllers/api-utils.ts
+++ src/application/http/controllers/api-utils.ts
@@ -7,23 +7,3 @@ export const idParamAsNumberSchema = z.object({
 export const idParamSchema = z.object({
   id: z.string().nonempty(),
 });
-
-export const fastifyDefaultErrorResponseSchema = (statusCode: number) =>
-  z.object({
-    statusCode: z.literal(statusCode),
-    error: z.string(),
-    message: z.string(),
-  });
-
-export type FastifyDefaultErrorResponseSchema = ReturnType<typeof fastifyDefaultErrorResponseSchema>;
-
-export const buildFastifyDefaultErrorResponses = <C extends number>(
-  statusCodes: C[],
-): { [K in C]: FastifyDefaultErrorResponseSchema } => {
-  return Object.fromEntries(
-    statusCodes.map((statusCode) => {
-      return [statusCode, fastifyDefaultErrorResponseSchema(statusCode)] as const;
-    }),
-    // cast as unknown because fromEntries will return as string key
-  ) as unknown as { [K in C]: FastifyDefaultErrorResponseSchema };
-};
